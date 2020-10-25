1e13189f49db8fbf7b05c006a4a00833d1ba97f0
Kai Moritz <kai@juplo.de>
2020-10-25 16:53:06 +0100

    TODO

--- src/main/java/de/juplo/kafka/outbox/OutboxRepository.java
+++ src/main/java/de/juplo/kafka/outbox/OutboxRepository.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 import java.time.ZonedDateTime;
 
@@ -13,6 +14,8 @@ import java.time.ZonedDateTime;
 @AllArgsConstructor
 public class OutboxRepository
 {
+  private static final String SQL_QUERY =
+      "SELECT id, key, value FROM outbox WHERE id > ?";
   private static final String SQL_UPDATE =
       "INSERT INTO outbox (key, value, issued) VALUES (:key, :value, :issued)";
 
@@ -26,5 +29,21 @@ public class OutboxRepository
     parameters.addValue("value", value);
     parameters.addValue("issued", Timestamp.from(issued.toInstant()));
     jdbcTemplate.update(SQL_UPDATE, parameters);
+  }
+
+  public List<OutboxItem> fetch(@NotNull Long sequenceNumber)
+  {
+    return
+        jdbcTemplate.query(SQL_QUERY, (resultSet, rowNumber) ->
+            {
+              return
+                  OutboxItem
+                      .builder()
+                      .sequenceNumber(resultSet.getLong(0))
+                      .key(resultSet.getString(1))
+                      .value(resultSet.getString(2))
+                      .build();
+            },
+            sequenceNumber);
   }
 }
