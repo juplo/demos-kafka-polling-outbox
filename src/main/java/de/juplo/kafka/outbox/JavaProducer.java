395a7c4faeedb3ce15e61736fa495c4976f5f096
Kai Moritz <kai@juplo.de>
2020-10-26 19:42:25 +0100

    WIP

--- src/main/java/de/juplo/kafka/outbox/JavaProducer.java
+++ src/main/java/de/juplo/kafka/outbox/JavaProducer.java
@@ -2,43 +2,67 @@ package de.juplo.kafka.outbox;
 
 import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PreDestroy;
 
 
 @Component
-public class JavaProducer implements ApplicationRunner
+public class JavaProducer
 {
   final static Logger LOG = LoggerFactory.getLogger(JavaProducer.class);
 
 
-  @Override
-  public void run(ApplicationArguments args) throws Exception
+  private final OutboxRepository repository;
+  private final KafkaProducer<String, String> producer;
+
+  private long sequenceNumber = 0l;
+
+  public JavaProducer(OutboxRepository repository)
   {
+    this.repository = repository;
+
     Properties props = new Properties();
     props.put("bootstrap.servers", "kafka:9092");
     props.put("metadata.max.age.ms", 1000);
     props.put("linger.ms", 20);
-    props.put("key.serializer", LongSerializer.class.getName());
+    props.put("key.serializer", StringSerializer.class.getName());
     props.put("value.serializer", StringSerializer.class.getName());
 
-    KafkaProducer<Long, String> producer = new KafkaProducer<>(props);
+    this.producer = new KafkaProducer<>(props);
+  }
 
-    try
+  @Scheduled(fixedDelay = 500)
+  public void poll()
+  {
+    List<OutboxItem> items;
+    do
     {
-      for (long i = 0; true; i++)
-      {
-        final ProducerRecord<Long, String> record
-            = new ProducerRecord<>("test", null, "Hallo Welt #" + i);
+      items = repository.fetch(sequenceNumber);
+      for (OutboxItem item : items)
+        send(item);
+    }
+    while (items.size() > 0);
+  }
 
+  void send(OutboxItem item)
+  {
+    final ProducerRecord<String, String> record =
+        new ProducerRecord<>("test", item.getKey(), item.getValue());
 
-        producer.send(record, (metadata, e) ->
+    producer.send(record, (metadata, e) ->
         {
           if (e == null)
           {
@@ -57,15 +81,11 @@ public class JavaProducer implements ApplicationRunner
             LOG.error("P - ERROR {}/{}: {}", record.topic(), record.partition(), e.toString());
           }
         });
+  }
 
-        Thread.sleep(100);
-      }
-    }
-    finally
-    {
-      LOG.info("P - Closing...");
-      producer.close();
-      LOG.info("P - DONE!");
-    }
+  @PreDestroy
+  public void close()
+  {
+    producer.close(5, TimeUnit.SECONDS);
   }
 }
