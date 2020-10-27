3bb8b3270cd263510d99151009b4e4d26359a035
Kai Moritz <kai@juplo.de>
2020-10-27 19:03:32 +0100

    WIP

--- src/main/java/de/juplo/kafka/outbox/Application.java
+++ src/main/java/de/juplo/kafka/outbox/Application.java
@@ -3,11 +3,29 @@ package de.juplo.kafka.outbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.CountDownLatch;
+
+
 @SpringBootApplication
 public class Application
 {
-  public static void main(String[] args)
+  public static void main(String[] args) throws Exception
   {
     SpringApplication.run(Application.class, args);
+
+    final CountDownLatch closeLatch = new CountDownLatch(1);
+
+    Runtime
+        .getRuntime()
+        .addShutdownHook(new Thread()
+        {
+          @Override
+          public void run()
+          {
+            closeLatch.countDown();
+          }
+        });
+
+    closeLatch.await();
   }
 }
