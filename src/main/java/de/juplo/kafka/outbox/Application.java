af09d7dd19d32d48e285959a424a37671057f1f8
Kai Moritz <kai@juplo.de>
2020-10-28 18:31:56 +0100

    Adding @EnableScheduling also keeps the application alive

--- src/main/java/de/juplo/kafka/outbox/Application.java
+++ src/main/java/de/juplo/kafka/outbox/Application.java
@@ -1,34 +1,16 @@
 package de.juplo.kafka.outbox;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.concurrent.CountDownLatch;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
-@Slf4j
+@EnableScheduling
 public class Application
 {
   public static void main(String[] args) throws Exception
   {
     SpringApplication.run(Application.class, args);
-
-    final CountDownLatch closeLatch = new CountDownLatch(1);
-
-    Runtime
-        .getRuntime()
-        .addShutdownHook(new Thread()
-        {
-          @Override
-          public void run()
-          {
-            log.info("Closing application...");
-            closeLatch.countDown();
-          }
-        });
-
-    closeLatch.await();
   }
 }
