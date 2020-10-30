bdc7b089e14ddc16f7e76f6a736b27b608e35ab4
Kai Moritz <kai@juplo.de>
2020-10-30 11:57:01 +0100

    Added Google Guava for byte-conversions

--- outbox/src/main/java/de/juplo/kafka/outbox/OutboxProducer.java
+++ outbox/src/main/java/de/juplo/kafka/outbox/OutboxProducer.java
@@ -1,8 +1,8 @@
 package de.juplo.kafka.outbox;
 
+import com.google.common.primitives.Longs;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.nio.ByteBuffer;
 import java.time.Duration;
 import java.util.List;
 import java.util.Properties;
@@ -64,9 +64,7 @@ public class OutboxProducer
         new ProducerRecord<>(topic, item.getKey(), item.getValue());
 
     sequenceNumber = item.getSequenceNumber();
-    ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
-    buffer.putLong(item.getSequenceNumber());
-    record.headers().add("SEQ#", buffer.array());
+    record.headers().add("SEQ#", Longs.toByteArray(sequenceNumber));
 
     producer.send(record, (metadata, e) ->
     {
